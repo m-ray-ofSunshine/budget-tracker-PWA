151358883271a519358aad8c6350d981d5932f55
mray1995 <sleepingbag5927@gmail.com>
2021-08-10 22:35:27 -0500

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const port = 3000;
 
 const app = express();
 
@@ -25,6 +25,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen((process.env.PORT || port), () => {
+  console.log(`A)pp running on port ${PORT}!`);
 });
